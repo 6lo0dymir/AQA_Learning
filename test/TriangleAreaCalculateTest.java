4b3ae305e2579595859e2262677efba289fee298
6lo0dymir <kennymc666@gmail.com>
2026-02-15 11:05:50 +0300

    New update

--- test/TriangleAreaCalculateTest.java
+++ test/TriangleAreaCalculateTest.java
@@ -1,5 +1,5 @@
-import org.testng.annotations.*;
 import org.testng.Assert;
+import org.testng.annotations.*;
 
 public class TriangleAreaCalculateTest {
     @BeforeClass
@@ -22,11 +22,41 @@ public class TriangleAreaCalculateTest {
         System.out.println("Тест завершен");
     }
 
-    @Test(priority = 1, description = "Правильный треугольник")
-       public void testTriangleExist(){
-        double result = TriangleAreaCalculate.calculateByHeron(10,12,20);
-        Assert.assertEquals(result,45.59D);
+    @Test(priority = 1, description = "Тест 1. Прямоугольный треугольник")
+       public void testTriangle3_4_5(){
+        double result = TriangleAreaCalculate.calculateByHeron(3,4,5);
+        Assert.assertEquals(result,6.0,0.01);
     }
+    @Test(priority = 2, description = "Тест 2. Равнобедренный треугольник")
+    public void testEqualSidesTriangle(){
+        double result = TriangleAreaCalculate.calculateByHeron(5,5,6);
+        Assert.assertEquals(result, 12.0,0.01);
+    }
+    @Test(priority = 3, description = "Тест 3. Равносторонний треугольник")
+    public void testAllSidesAreEqual(){
+        double result = TriangleAreaCalculate.calculateByHeron(6,6,6);
+        Assert.assertEquals(result, 15.58,0.01);
+    }
+    @Test(priority = 4, description = "Тест 4. Минимальные стороны")
+    public void testMinimalSides(){
+        double result = TriangleAreaCalculate.calculateByHeron(0.0001,0.0001,0.0001);
+        Assert.assertEquals(result, 0,0.000001);
+    }
+    @Test(priority = 5, description = "Тест 5. Треугольник с нулевой стороной")
+    public void testZeroSide(){
+        Assert.assertThrows(IllegalArgumentException.class, ()-> TriangleAreaCalculate.calculateByHeron(10,12,0));
+    }
+    @Test(priority = 6, description = "Тест 6. Отрицательная сторона")
+    public void testNegativeSide(){
+        Assert.assertThrows(IllegalArgumentException.class, ()-> TriangleAreaCalculate.calculateByHeron(-10,12,20));
+    }
+    @Test(priority = 7, description = "Тест 7. Несуществующий треугольник")
+    public void testTriangleNotExist(){
+        Assert.assertThrows(IllegalArgumentException.class, ()-> TriangleAreaCalculate.calculateByHeron(1,2,5));
+    }
+
+
+
 
 
 }
