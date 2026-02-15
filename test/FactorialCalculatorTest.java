f278bf95556acf31d3025dc8340f6a1fbbfad2d1
6lo0dymir <kennymc666@gmail.com>
2026-02-15 09:43:56 +0300

    New update

--- test/FactorialCalculatorTest.java
+++ test/FactorialCalculatorTest.java
@@ -1,10 +1,21 @@
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import static org.testng.Assert.*;
 
 public class FactorialCalculatorTest {
-    @BeforeClass(){
-        System.out.println("Начало тестирования FactorialCalculatorT");
+    @BeforeClass()
+    public void setUp(){
+        System.out.println("Начало тестирования FactorialCalculator");
     }
+    @AfterClass()
+    public void tearDown(){
+        System.out.println("Завершение тестирования FactorialCalculator");
+    }
+    @BeforeMethod()
+    public void
 
 }
