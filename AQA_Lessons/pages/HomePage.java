89b20b98b9ee7aada90ea762e79df411a644f65a
6lo0dymir <kennymc666@gmail.com>
2026-02-23 10:31:29 +0300

    New update

---
+++ AQA_Lessons/pages/HomePage.java
@@ -0,0 +1,12 @@
+package pages;
+
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.*;
+
+public class HomePage {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+
+}
