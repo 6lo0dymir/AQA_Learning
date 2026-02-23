e9d29bcae4b944fe48583999ba8a0a8709a5cbf9
6lo0dymir <kennymc666@gmail.com>
2026-02-23 11:26:21 +0300

    New update

--- AQA_Lessons/pages/PaymentPopUpWindow.java
+++ AQA_Lessons/pages/PaymentPopUpWindow.java
@@ -7,14 +7,14 @@ public class PaymentPopUpWindow {
     WebDriver driver;
     WebDriverWait wait;
 
-    private By iframe = By.xpath("//div[@class='pay__wrapper']//iframe");
+    private By iframe = By.xpath("//div[@class='payment-widget-app']//iframe[contains(@src, 'checkout']");
     private By amountOnCard = By.xpath("//div[@class='card-page__card']//span[@class='card-page__inner']");
     private By amountOnButton = By.xpath("//button[contains(@class, 'card-page__button')]//span[contains(@class, 'card-page__inner')]");
     private By phoneNumber = By.xpath("//div[@class='card-page__card']//div[@class='card-page__phone']");
 
     private By cardNumberField = By.id("cc-number");
     private By expiryDateField = By.id("cc-exp");
-    private By cvvField = By.id("cc-cvv");
+    private By cvvField = By.id("cc-csc");
 
     private By visaIcon = By.xpath("//img[contains(@src, 'visa-system.svg')]");
     private By mastercardIcon = By.xpath("//img[contains(@src, 'mastercard-system.svg')]");
